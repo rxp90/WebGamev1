cf8ba4854e6ce1a6af8aac4d413f4ebde7025ea6
Raúl García <garciacalvoraul@gmail.com>
2012-07-25 03:20:51 +0200

    Doble buffering para evitar parpadeos al redibujar elementos.

--- WebGame/src/StartingPoint.java
+++ WebGame/src/StartingPoint.java
@@ -1,6 +1,7 @@
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 
 public class StartingPoint extends Applet implements Runnable {
 
@@ -9,16 +10,35 @@ public class StartingPoint extends Applet implements Runnable {
 	private int dx = 1;
 	private int dy = 1;
 	private int radius = 20;
+	private Image image;
+	private Graphics doubleG;
 
 	@Override
 	public void init() {
-		setSize(800,600);
+		setSize(800, 600);
 	}
 
 	@Override
 	public void start() {
 		Thread thread = new Thread(this);
 		thread.start();
+	}
+
+	@Override
+	public void update(Graphics g) {
+		// Double buffering.
+		if (image == null) {
+			image = createImage(this.getSize().width, this.getSize().height);
+			doubleG = image.getGraphics();
+		}
+		
+		doubleG.setColor(getBackground());
+		doubleG.fillRect(0, 0, this.getSize().width, this.getSize().height);
+		
+		doubleG.setColor(getForeground());
+		paint(doubleG);
+		
+		g.drawImage(image, 0, 0, this);
 	}
 
 	@Override
